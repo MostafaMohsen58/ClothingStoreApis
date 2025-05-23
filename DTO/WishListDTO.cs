dd50e8ac05b7d2a40e72cc0b8f2f9a187a3115c7
MostafaMohsen58 <mostafa.elnahas5884@gmail.com>
2025-05-23 03:27:13 +0300

    Add Full Project files

---
+++ DTO/WishListDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace ClothingAPIs.DTO
+{
+    public class WishListDTO
+    {
+
+        public int PrdId { get; set; }
+        //public string PrdName { get; set; }
+        //public string? PrdImg { get; set; }
+        //public string? PrdDes { get; set; }
+        public string? UserId { get; set; }
+
+
+    }
+    public class AddWishListDTO
+    {
+        public int prodid { get; set; }
+    }
+}
---
+++ IRepoServices/IWishListService.cs
@@ -0,0 +1,13 @@
+﻿using ClothingAPIs.Models;
+using ClothingAPIs.DTO;
+
+namespace ClothingAPIs.IRepoServices
+{
+	public interface IWishListService
+	{
+		public void Create(int ProdId, string userId);
+		public void DeleteWishList(int ProdId, string userId);
+		public List<int> GetWishListByUserId(string userId);
+
+	}
+}
